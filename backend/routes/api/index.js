64e444efd93799b98f7d2a512c57f727778c1263
Jacob North <yakenorth@protonmail.com>
2022-03-03 16:41:27 -0500

    Added user routes to api

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,48 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
-router.post('/test', function (req, res) {
+router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
 
-
 module.exports = router;
+
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+
+// // Test for token cookie
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
+
+// // Test restore user
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// // test require auth
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
