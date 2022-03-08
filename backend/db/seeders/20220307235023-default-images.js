3e126d31a38c40f50d9bd93e845395bb12e0fe68
Jacob North <yakenorth@protonmail.com>
2022-03-07 21:47:21 -0500

    Added image seeds

--- backend/db/seeders/20220307235023-default-images.js
+++ backend/db/seeders/20220307235023-default-images.js
@@ -2,25 +2,51 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Images', [
+      {
+        spotId: 1,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-1.jpg'
+      },
+      {
+        spotId: 2,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-2.jpg'
+      },
+      {
+        spotId: 3,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-4.jpg'
+      },
+      {
+        spotId: 4,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-5.jpg'
+      },
+      {
+        spotId: 5,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-6.jpg'
+      },
+      {
+        spotId: 6,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-7.jpg'
+      },
+      {
+        spotId: 7,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-8.jpg'
+      },
+      {
+        spotId: 8,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-10.jpg'
+      },
+      {
+        spotId: 9,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-11.jpg'
+      },
+      {
+        spotId: 10,
+        url: 'https://www.impressiveinteriordesign.com/wp-content/uploads/2014/11/Bathroom-Interior-Design-Photo-Gallery-With-Gorgeous-Examples-12.jpg'
+      },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('Images', null, {});
   }
 };
