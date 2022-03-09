a2a198dcc531c4ad4df1fcc956936b1d3060cdb0
Jacob North <yakenorth@protonmail.com>
2022-03-09 04:29:04 -0500

    Added more styling to lists page

--- frontend/src/components/Spots/SpotsList/SpotListing/index.js
+++ frontend/src/components/Spots/SpotsList/SpotListing/index.js
@@ -8,7 +8,10 @@ const SpotListing = ({ details }) => {
             <Link to={`/spots/${details.id}`} className='listing-card-link' >
                 <div className='listing-card-background'>
                     <img src={details.Images[0] ? details.Images[0].url : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7pqzMM1bTqxAEkWQ3YHT4pdXByPaZQRYzmA&usqp=CAU'} alt='spot pic' />
-                    <div>{details.name}</div>
+                    <div className='listing-details-container'>
+                        <span className='listing-name' >{details.name}</span>
+                        <span className='listing-price' >{`$${details.price} / 5 min`}</span>
+                    </div>
                 </div>
             </Link>
         </div>
