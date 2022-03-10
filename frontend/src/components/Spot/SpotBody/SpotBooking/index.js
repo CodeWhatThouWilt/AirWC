3805e0bdcef7083f2b1615a3e78ababae831e2ea
Jacob North <yakenorth@protonmail.com>
2022-03-10 03:55:02 -0500

    Added react-calendar

--- frontend/src/components/Spot/SpotBody/SpotBooking/index.js
+++ frontend/src/components/Spot/SpotBody/SpotBooking/index.js
@@ -1,9 +1,13 @@
 import './SpotBooking.css'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
 const SpotBooking = () => {
 
     return (
-        <div className='spot-booking-container'>SpotBooking Component</div>
+        <div className='spot-booking-container'>
+            <Calendar />
+        </div>
     )
 }
 
