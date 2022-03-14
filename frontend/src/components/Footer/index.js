ace1449576dae0f0e1c7da9798a8a82b63cdeb25
Jacob North <yakenorth@protonmail.com>
2022-03-14 09:41:47 -0400

    Last styling add before presentation

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,13 +2,21 @@ import './Footer.css'
 import { DiReact, DiGithubBadge, DiJavascript1, DiVisualstudio } from 'react-icons/di';
 import { SiSequelize, SiCss3, SiExpress, SiPostgresql, SiRedux } from 'react-icons/si';
 import { AiFillHtml5 } from 'react-icons/ai';
-import { FaNodeJs } from 'react-icons/fa'
+import { FaNodeJs, FaToiletPaper } from 'react-icons/fa'
+import { useState } from 'react';
 
 
 const Footer = () => {
+    const [movement, setMovement] = useState(-60);
+
 
     return (
-        <div className='footer'>
+        <div className='footer' style={{ bottom: movement}}>
+            <div className='tab-holder'>
+                <div className='toilet-paper-container'  onClick={() => movement === -60 ? setMovement(0) : setMovement(-60)}>
+                    <FaToiletPaper />
+                </div>
+            </div>
             <SiRedux />
             <AiFillHtml5 />
             <SiCss3 />
