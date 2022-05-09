533b05a2ad570e2346a9a4dac11dd0c0ea6b1e9e
Jacob North <yakenorth@protonmail.com>
2022-05-09 19:01:09 -0400

    navbar styling changes

--- frontend/src/components/Homepage/Banner/index.js
+++ frontend/src/components/Homepage/Banner/index.js
@@ -5,11 +5,14 @@ import background from '../../../assets/banner-background.jpeg'
 const Banner = () => {
 
     return (
-        <div className='banner-container' style={{ backgroundImage: `url(${background})` }}>
-            <h1>We get it, you're on the go.</h1>
-            <h2>Public restrooms? Let's not.</h2>
-            <h3>Find a respectable bathroom near you.</h3>
-            <Link to='/spots'><button className='home-list-button' >Click here</button></Link>
+        <div className='banner-container'>
+            <img src={background} alt='banner' />
+            <div className='banner-txt'>
+                <h1>We get it, you're on the go.</h1>
+                <h2>Public restrooms? Let's not.</h2>
+                <h3>Find a respectable bathroom near you.</h3>
+                <Link to='/spots'><button className='home-list-button' >Click here</button></Link>
+            </div>
         </div>
     )
 }
