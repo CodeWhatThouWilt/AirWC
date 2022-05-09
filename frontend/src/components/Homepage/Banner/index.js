68b4b7d0fc92ef27aff7d7948aad9464b7df14c6
Jacob North <yakenorth@protonmail.com>
2022-05-09 16:54:40 -0400

    styling changes to splash page

--- frontend/src/components/Homepage/Banner/index.js
+++ frontend/src/components/Homepage/Banner/index.js
@@ -1,10 +1,11 @@
 import './Banner.css';
 import { Link } from 'react-router-dom';
+import background from '../../../assets/banner-background.jpeg'
 
 const Banner = () => {
 
     return (
-        <div className='banner-container'>
+        <div className='banner-container' style={{ backgroundImage: `url(${background})` }}>
             <h1>We get it, you're on the go.</h1>
             <h2>Public restrooms? Let's not.</h2>
             <h3>Find a respectable bathroom near you.</h3>
