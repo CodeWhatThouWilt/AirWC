5f6f79beddeea123838ed670a7e2721ee3b46a78
Jacob North <yakenorth@protonmail.com>
2022-05-09 22:09:03 -0400

    Finished up styling on spot lists

--- frontend/src/components/Spots/SpotListing/index.js
+++ frontend/src/components/Spots/SpotListing/index.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 const SpotListing = ({ details }) => {
     const images = Object.values(details.Images);
     return (
+        <div className='listing-card'>
             <Link to={`/spots/${details.id}`} className='listing-card-link' >
-                    <img src={images.length ? images[0].url : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7pqzMM1bTqxAEkWQ3YHT4pdXByPaZQRYzmA&usqp=CAU'} alt='spot pic' />
-                    <div className='listing-details-container'>
-                        <span className='listing-name' >{details.name}</span>
-                        <span className='listing-price' >{`$${details.price} / 5 min`}</span>
-                    </div>
+                <img src={images.length ? images[0].url : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7pqzMM1bTqxAEkWQ3YHT4pdXByPaZQRYzmA&usqp=CAU'} alt='spot pic' />
+                <div className='listing-details-container'>
+                    <span className='listing-name' >{details.name}</span>
+                    <div className='listing-price' ><span>{`$${details.price}`}</span> / 5 min</div>
+                </div>
             </Link>
+        </div>
     )
 }
 
