d0720ff9530c1b6f694fb52e09278bdfb7aa1545
Jacob North <yakenorth@protonmail.com>
2022-05-09 20:31:08 -0400

    Styling changes to listings

--- frontend/src/components/Spots/SpotListing/index.js
+++ frontend/src/components/Spots/SpotListing/index.js
@@ -4,17 +4,13 @@ import { Link } from 'react-router-dom';
 const SpotListing = ({ details }) => {
     const images = Object.values(details.Images);
     return (
-        <div className='listing-card' >
             <Link to={`/spots/${details.id}`} className='listing-card-link' >
-                <div className='listing-card-background'>
                     <img src={images.length ? images[0].url : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7pqzMM1bTqxAEkWQ3YHT4pdXByPaZQRYzmA&usqp=CAU'} alt='spot pic' />
                     <div className='listing-details-container'>
                         <span className='listing-name' >{details.name}</span>
                         <span className='listing-price' >{`$${details.price} / 5 min`}</span>
                     </div>
-                </div>
             </Link>
-        </div>
     )
 }
 
