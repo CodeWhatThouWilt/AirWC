91f6fd8ed84935d481c96df19dc7bdbd7a2253c3
Jacob North <yakenorth@protonmail.com>
2022-05-19 18:57:35 -0400

    Fixed issue with login failure

--- frontend/src/components/UserModal/CheckEmailForm/index.js
+++ frontend/src/components/UserModal/CheckEmailForm/index.js
@@ -37,7 +37,6 @@ function CheckEmailForm() {
         e.preventDefault();
         setErrors([]);
         return dispatch(sessionActions.login({ credential: 'user1@default.com', password: 'password' }))
-        .then(() => dispatch(getAllFavorites()))
         .catch(
             async (res) => {
                 const data = await res.json();
